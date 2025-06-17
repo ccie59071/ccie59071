f4768b4e91171b7b012a4f1be7ac0d3063220e5e
Christian Shematsi <cisco90cls@gmail.com>
2025-06-17 14:17:50 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,24 @@
+const CONFIG = {
+  formspreeEndpoint: 'https://formspree.io/f/{your-form-id}', // Replace with your Formspree endpoint
+  googleAnalyticsId: 'G-XXXXXXXXXX', // Replace with your Google Analytics ID (or leave empty to disable)
+  linkedinUrl: 'https://linkedin.com/in/your-profile', // Replace with your LinkedIn URL
+  xUrl: 'https://x.com/your-profile', // Replace with your X URL
+  phoneNumber: '+XXX-XXX-XXX-XXX' // Replace with your phone number
+};
+
+// Dynamically insert Google Analytics if ID is provided
+if (CONFIG.googleAnalyticsId) {
+  const gaScript = document.createElement('script');
+  gaScript.async = true;
+  gaScript.src = `https://www.googletagmanager.com/gtag/js?id=${CONFIG.googleAnalyticsId}`;
+  document.head.appendChild(gaScript);
+
+  const gaConfigScript = document.createElement('script');
+  gaConfigScript.innerHTML = `
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', '${CONFIG.googleAnalyticsId}');
+  `;
+  document.head.appendChild(gaConfigScript);
+}
